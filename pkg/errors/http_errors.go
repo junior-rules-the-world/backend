7c937340a4514a799936873d0361141d1fdc7110
Akavi <gabdushev.i@yandex.ru>
2024-03-09 15:55:42 +0500

    (chore) write http error func

---
+++ pkg/errors/http_errors.go
@@ -0,0 +1,44 @@
+package errors
+
+const (
+	EmailAlreadyUsed    = "EMAIL_ALREADY_USED"
+	UsernameAlreadyUsed = "USERNAME_ALREADY_USED"
+	NotFound            = "NOT_FOUND"
+	BadQuery            = "BAD_QUERY"
+	Forbidden           = "FORBIDDEN"
+	Unauthorized        = "UNAUTHORIZED"
+	WrongCredentials    = "WRONG_CREDENTIALS"
+	ServerError         = "INTERNAL_SERVER_ERROR"
+)
+
+type HttpErr interface {
+	Status() int
+	Error() string
+	Causes() interface{}
+}
+
+type HttpError struct {
+	StatusCode int         `json:"status_code,omitempty"`
+	Key        string      `json:"key,omitempty"`
+	Stack      interface{} `json:"-"`
+}
+
+func (err HttpError) Status() int {
+	return err.StatusCode
+}
+
+func (err HttpError) Error() string {
+	return err.Key
+}
+
+func (err HttpError) Causes() interface{} {
+	return err.Stack
+}
+
+func NewHttpError(status int, key string, stack interface{}) HttpError {
+	return HttpError{
+		StatusCode: status,
+		Key:        key,
+		Stack:      stack,
+	}
+}
